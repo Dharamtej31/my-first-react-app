15869f5c3ad34208f6be82dfb8507ef92759f8ae
Teja <dharamtej31@gmail.com>
2023-12-23 16:09:01 +0530

    my app
    react-app

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+  
+    <div className="App">
+     
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <div className='body'>
+        <h1>Hello Teja </h1>
+        <img src='./assets/teja.jpg' alt='no image' className='img1' />
+        <h1>Hello Sharath</h1>
+        <img src='./assets/sarath.jpg' alt='no image' className='img2' />
+        <h3>Hello Eswar</h3>
+       
+        </div>
+        
+        
+        
+       
+    </div>
+    </>
+  );
+}
+
+export default App;
